f008481b12c2193994f96932b6048050f155564b
HEYITSGILBERT780 <gilbertaragon7801@gmail.com>
2016-01-30 12:11:05 -0800

    Menu habit list

--- src/app.js
+++ src/app.js
@@ -5,59 +5,30 @@
  */
 
 var UI = require('ui');
-var ajax= require('ajax');
-var Vector2 = require('vector2');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.',
-  subtitleColor: 'indigo', // Named colors
-  bodyColor: '#9a0036' // Hex colors
+// Make a list of menu items
+var habits = [
+  {
+    title: "Studying",
+    subtitle: "Learn."
+  },
+  {
+    title: "Fitness",
+    subtitle: "Don't be weak."
+  },
+  {
+    title: "Hygiene",
+    subtitle: "Don't smell bad."
+  }
+];
+
+// Create the Menu, supplying the list of habits
+var habitMenu = new UI.Menu({
+  sections: [{
+    title: 'Habits',
+    items: habits
+  }]
 });
 
-main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+// Show the Menu
+habitMenu.show();
