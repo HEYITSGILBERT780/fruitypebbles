50d88337f683f7376cd307d9403567739ed020c0
HEYITSGILBERT780 <gilbertaragon7801@gmail.com>
2016-01-30 16:59:53 -0800

    Still a rough draft

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 /**
- * Welcome to Hackucsc2016!
+ * Welcome to Pebble.js!
  *
  * This is where you write your app.
  */
 
 var UI = require('ui');
+var health = 100;
+var exp = 0;
 
 // Make a list of menu items
 var habits = [
@@ -65,6 +67,52 @@ habitMenu.on('select', function(event) {
     
     // Show the new Menu
     studyMenu.show();
+    
+    if (study[event.itemIndex].title == "Read") {
+      // Add a click listener for select button click
+      studyMenu.on('select', function(event) {
+
+        // Show a card with clicked item details
+        var studyCard = new UI.Card({
+          title: study[event.itemIndex].title,
+          body: study[event.itemIndex].subtitle + "\n" + "\nhealth: " + health + "\nexp: " + exp
+        });
+
+        // Show the new Card
+        studyCard.show();
+        
+        studyCard.on('click', 'up', function(event){
+          exp += 5;
+        });
+        
+        studyCard.on('click', 'down', function(event){
+          health -= 3;
+        });
+      });
+    }
+    
+    if (study[event.itemIndex].title == "Homework") {
+      // Add a click listener for select button click
+      studyMenu.on('select', function(event) {
+        
+        // Show a card with clicked item details
+        var studyCard = new UI.Card({
+          title: study[event.itemIndex].title,
+          body: study[event.itemIndex].subtitle + "\n" + "\nhealth: " + health + "\nexp: " + exp
+        });
+        
+        //Show the new Card
+        studyCard.show();
+        
+        studyCard.on('click', 'up', function(event) {
+          exp += 5;
+        });
+        
+        studyCard.on('click', 'down', function(event) {
+          health -= 3;
+        });
+      });
+    }
   }
   
   if (habits[event.itemIndex].title == "Fitness") {
@@ -85,7 +133,7 @@ habitMenu.on('select', function(event) {
       },
       {
         title: "Sports",
-        subtitle: "Play any sports"
+        subtitle: "Play any sports?"
       }
     ];
     
@@ -99,6 +147,89 @@ habitMenu.on('select', function(event) {
     
     // Show the new Menu
     fitMenu.show();
+    
+    if (fit[event.itemIndex].title == "Workout") {
+      // Add a click listener for select button click
+      fitMenu.on('select', function(event) {
+
+        // Show a card with clicked item details
+        var fitCard = new UI.Card({
+          title: fit[event.itemIndex].title,
+          body: fit[event.itemIndex].subtitle + "\n" + "\nhealth: " + health + "\nexp: " + exp
+        });
+
+        // Show the new Card
+        fitCard.show();
+        
+        fitCard.on('click', 'up', function(event) {
+          exp += 5;
+        });
+        
+        fitCard.on('click', 'up', function(event) {
+          health -= 3;
+        });
+      });
+    }
+    
+    if (fit[event.itemIndex].title == "Run") {
+      fitMenu.on('select', function(event) {
+        
+        var fitCard = new UI.Card({
+          title: fit[event.itemIndex].title,
+          subtitle: fit[event.itemIndex].subtitle + "\n" + "\nhealth: " + health + "\nexp: " + exp
+        });
+        
+        fitCard.show();
+        
+        fitCard.on('click', 'up', function(event) {
+          exp += 5;
+        });
+        
+        fitCard.on('click', 'up', function(event) {
+          health -= 3;
+        });
+      });
+    }
+    
+    if (fit[event.itemIndex].title == "Walk") {
+      fitMenu.on('select', function(event) {
+        
+        var fitCard = new UI.Card({
+          title: fit[event.itemIndex].title,
+          body: fit[event.itemIndex].subtitle + "\n" + "\nhealth: " + health + "\nexp: " + exp 
+        });
+        
+        fitCard.show();
+        
+        fitCard.on('click', 'up', function(event) {
+          exp += 5;
+        });
+        
+        fitCard.on('click', 'up', function(event) {
+          health -= 3;
+        });
+      });
+    }
+    
+    if (fit[event.itemIndex].title == "Sports") {
+      fitMenu.on('select', function(event) {
+         
+        var fitCard = new UI.Card({
+          title: fit[event.itemIndex].title,
+          body: fit[event.itemIndex].subtitle + "\n" + "\nhealth: " + health + "\exp: " + exp
+        });
+      
+        fitCard.show();
+        
+        fitCard.on('click', 'up', function(event) {
+          exp += 5;
+        });
+        
+        fitCard.on('click', 'down', function(event) {
+          health -= 3;
+        });
+      });
+    }
   }
   
   if (habits[event.itemIndex].title == "Hygiene") {
@@ -107,7 +238,7 @@ habitMenu.on('select', function(event) {
     var hyg = [
       {
         title: "Shower",
-        subtitle: "Did you take a shower"
+        subtitle: "Did you take a shower?"
       },
       {
         title: "Brush",
@@ -129,6 +260,66 @@ habitMenu.on('select', function(event) {
     
     // Show the new Menu
     hygMenu.show();
+    
+    if (hyg[event.itemIndex].title == "Shower") {
+      hygMenu.on('select', function(event) {
+        
+        var hygCard = new UI.Card ({
+          title: hyg[event.itemIndex].title,
+          body: hyg[event.itemIndex].subtitle + "\n" + "\nhealth: " + health + "\nexp: " + exp 
+        });
+        
+        hygCard.show();
+        
+        hygCard.on('click', 'up', function(event) {
+          exp += 5;
+        });
+        
+        hygCard.on('click', 'down', function(event) {
+          health -= 3;
+        });
+      });
+    }
+    
+    if (hyg[event.itemIndex].title == "Brush") {
+      hygMenu.on('select', function(event) {
+        
+        var hygCard = new UI.Card ({
+          title: hyg[event.itemIndex].title,
+          body: hyg[event.itemIndex].subtitle + "\n" + "\nhealth: " + health + "\nexp: " + exp
+        });
+        
+        hygCard.show();
+        
+        hygCard.on('click', 'up', function(event) {
+          exp += 5;
+        });
+        
+        hygCard.on('click', 'down', function(event) {
+          health -= 3;
+        });
+      });
+    }
+    
+    if (hyg[event.itemIndex].title == "Floss") {
+      hygMenu.on('select', function(event) {
+        
+        var hygCard = new UI.Card({
+          title: hyg[event.itemIndex].title,
+          body: hyg[event.itemIndex].subtitle + "\n" + "\nhealth: " + health + "\nexp: " + exp
+        });
+        
+        hygCard.show();
+        
+        hygCard.on('click', 'up', function(event) {
+          exp += 5;
+        });
+        
+        hygCard.on('click', 'down', function(event) {
+          health -= 3;
+        });
+      });
+    }
   }
   
   if (habits[event.itemIndex].title == "Chores") {
@@ -163,5 +354,85 @@ habitMenu.on('select', function(event) {
     
     // Show the new Menu
     choreMenu.show();
+    
+    if (chore[event.itemIndex].title == "Make bed") {
+      choreMenu.on('select', function(event) {
+        
+        var choreCard = new UI.Card ({
+          title: chore[event.itemIndex].title,
+          body: chore[event.itemIndex].subtitle + "\n" + "\nhealth: " + health + "\nexp: " + exp
+        });
+        
+        choreCard.show();
+        
+        choreCard.on('click', 'up', function(event) {
+          exp += 5;
+        });
+        
+        choreCard.on('click', 'down', function(event) {
+          health -= 3;
+        });
+      });
+    }
+    
+    if (chore[event.itemIndex].title == "Dishes") {
+      choreMenu.on('select', function(event) {
+        
+        var choreCard = new UI.Card ({
+          title: chore[event.itemIndex].title,
+          body: chore[event.itemIndex].subtitle + "\n" + "\nhealth: " + health + "\exp: " + exp
+        });
+        
+        choreCard.show();
+        
+        choreCard.on('click', 'up', function(event) {
+          exp += 5;
+        });
+        
+        choreCard.on('click', 'down', function(event) {
+          health -= 3;
+        });
+      });
+    }
+    
+    if (chore[event.itemIndex].title == "Clean room") {
+      choreMenu.on('select', function(event) {
+        
+        var choreCard = new UI.Card ({
+          title: chore[event.itemIndex].title,
+          body: chore[event.itemIndex].subtitle + "\n" + "\nhealth: " + health + "\exp: " + exp
+        });
+        
+        choreCard.show();
+        
+        choreCard.on('click', 'up', function(event) {
+          exp += 5;
+        });
+        
+        choreCard.on('click', 'down', function(event) {
+          health -= 3;
+        });
+      });
+    }
+    
+    if (chore[event.itemIndex].title == "Laundry") {
+      choreMenu.on('select', function(event) {
+        
+        var choreCard = new UI.Card({
+          title: chore[event.itemIndex].title,
+          body: chore[event.itemIndex].subtitle + "\n" + "\nhealth: " + health + "\nexp: " + exp
+        });
+        
+        choreCard.show();
+        
+        choreCard.on('click', 'up', function(event) {
+          exp += 5;
+        });
+        
+        choreCard.on('click', 'down', function(event) {
+          health -= 3;
+        });
+      });
+    }
   }
 });
